98b80f6448e8059e00a21852d6d229da43829d1e
Martin Nestorov <mnestorov@protonmail.com>
2019-04-05 22:29:18 +0300

    Adds green output at the end

--- src/screen.cpp
+++ src/screen.cpp
@@ -1,3 +1,5 @@
+#include <thread>
+#include <chrono>
 #include "screen.hpp"
 
 namespace screen
@@ -6,5 +8,15 @@ namespace screen
     {
 	SDL_SetRenderDrawColor(r, 0x00, 0x00, 0x00, 0x00);
 	SDL_RenderClear(r);
+    }
+
+    void finish(SDL_Renderer* r, std::vector<SDL_Rect> &array)
+    {
+	for (size_t i = 0; i < array.size(); i++) {
+	    SDL_SetRenderDrawColor(r, 0x00, 0xFF, 0x00, 0xFF);
+	    SDL_RenderFillRect(r, &array[i]);
+	    SDL_RenderPresent(r);
+	    std::this_thread::sleep_for(std::chrono::milliseconds(05));
+	}
     }
 }
--- src/screen.hpp
+++ src/screen.hpp
@@ -2,11 +2,12 @@
 #define __SCREEN_H_INCLUDED__
 
 #include <SDL2/SDL.h>
+#include <vector>
 
 namespace screen
 {
     void clear(SDL_Renderer* r);
-    //void render(SDL_Renderer* r, std::vector<SDL_Rect> &array, int left);
+    void finish(SDL_Renderer* r, std::vector<SDL_Rect> &array);
 }
 
 #endif

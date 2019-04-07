331b3fc7ba1da5b0eac49c8b0d86f3fcce88c5fe
Martin Nestorov <mnestorov@protonmail.com>
2019-04-07 15:52:32 +0300

    Adds sequential random set

--- src/number_set.cpp
+++ src/number_set.cpp
@@ -30,6 +30,28 @@ namespace number_set
 	return pillars;
     }
 
+    std::vector<SDL_Rect> seq_random()
+    {
+	int pillar_width = 1;
+	int num_pillars = screen::WIDTH / pillar_width;
+	std::vector<int> pillar_vals;
+	for (int i = 0; i < num_pillars; i++) {
+	    double frac = (double)screen::HEIGHT / screen::WIDTH;
+	    int pillar_val = std::floor(frac * i);
+	    pillar_vals.push_back(pillar_val);
+	}
+	std::random_shuffle(pillar_vals.begin(), pillar_vals.end());
+	std::vector<SDL_Rect> pillars;
+	for (int i = 0; i < num_pillars; i++) {
+	    int rect_y = screen::HEIGHT - pillar_vals[i];
+
+	    SDL_Rect rect = {i * pillar_width, rect_y, pillar_width, pillar_vals[i]};
+
+	    pillars.push_back(rect);
+	}
+	return pillars;
+    }
+
     std::vector<SDL_Rect> reversed()
     {
 	int pillar_width = 1;
@@ -38,7 +60,7 @@ namespace number_set
 	std::vector<SDL_Rect> pillars;
 	for (int i = 0; i < num_pillars; i++) {
 	    double frac = (double)screen::HEIGHT / screen::WIDTH;
-	    int pillar_val = std::floor((frac) * (num_pillars - i));
+	    int pillar_val = std::floor(frac * (num_pillars - i));
 	    int rect_y = screen::HEIGHT - pillar_val;
 
 	    SDL_Rect rect = {i * pillar_width, rect_y, pillar_width, pillar_val};
--- src/number_set.hpp
+++ src/number_set.hpp
@@ -2,6 +2,7 @@
 #define __NUMBER_SET_H_INCLUDED__
 
 #include <random>
+#include <algorithm>
 #include <vector>
 
 #include <SDL2/SDL.h>
@@ -10,6 +11,7 @@ namespace number_set
 {
     int gen();
     std::vector<SDL_Rect> random();
+    std::vector<SDL_Rect> seq_random();
     //std::vector<SDL_Rect> nearly_sorted();
     std::vector<SDL_Rect> reversed();
     //std::vector<SDL_Rect> few_unique();
